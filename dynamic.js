65d588944609641484f93bc2cd499327f3bf05de
Archanacharvi <152805997+Archanacharvi@users.noreply.github.com>
2023-12-27 13:48:01 +0530

    Add files via upload

---
+++ dynamic.js
@@ -0,0 +1,10 @@
+// You can add interactive features using JavaScript if needed
+// For simplicity, this example only includes a console log
+
+document.getElementById('image1').addEventListener('click', function() {
+    console.log('Image 1 clicked!');
+});
+
+document.getElementById('image2').addEventListener('click', function() {
+    console.log('Image 2 clicked!');
+});
